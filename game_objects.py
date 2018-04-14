ee3e8349479027274184d089590c582f24da7f4d
HactarCE <6060305+HactarCE@users.noreply.github.com>
2018-04-14 17:22:39 -0400

    Added handle, implemented rotation, and optimized rendering

--- game_objects.py
+++ game_objects.py
@@ -4,15 +4,21 @@ import layers
 import text_engine
 import utils
 
+# TODO fix blitting logic
+
 class GameObject(object):
     def __init__(self, surface):
         self.parent = None
         self.surf = surface
+        self.comp_surf = surface.copy()
+        self.old_rect = None
         self.rect = surface.get_rect()
         self.children = []
         self.layers = [[] for i in range(layers.LAYER_COUNT)]
-        self.need_blit = True
+        self.invalidated_rects = []
+        self.invalidate()
 
+    #region Movement
     def move(self, offset):
         self.rect.move_ip(offset)
 
@@ -22,7 +28,9 @@ class GameObject(object):
     def move_center_to(self, pos):
         new_pos = tuple(pos[i] - self.rect[i + 2] // 2 for i in range(2))
         self.rect = self.surf.get_rect().move(new_pos)
+    #endregion
 
+    #region Children
     def add_child(self, child, layer=1):
         if child not in self.children:
             self.children.append(child)
@@ -36,7 +44,17 @@ class GameObject(object):
             if child in layer:
                 layer.remove(child)
         child.parent = None
+    #endregion
 
+    #region Events
+    def handle_event(self, ev):
+        handled = False
+        for child in self.children:
+            handled = child.handle_event(ev) or handled
+        return handled
+    #endregion
+
+    #region Updates
     def pre_update(self):
         for child in self.children:
             child.pre_update()
@@ -48,36 +66,46 @@ class GameObject(object):
     def post_update(self):
         for child in self.children:
             child.post_update()
+    #endregion
+
+    #region Drawing
+    def invalidate(self, area=None):
+        # invlaidated_rects is a list of a Rects in local space
+        if not area:
+            area = self.surf.get_rect()
+        if self.parent:
+            self.parent.invalidate(area.move(self.rect.topleft))
+        else:
+            # TODO maybe add logic for combining Rects, or at least removing
+            #      Rects that are completely contained by others
+            self.invalidated_rects.append(area)
 
     def redraw_if_needed(self):
-        updated_rects = []
-        for layer in self.layers:
-            for child in layer:
-                if child.rect.collidelist(updated_rects) != -1:
-                    child.invalidate()
-                if child.redraw_if_needed():
-                    updated_rects.append(child.rect)
-        if self.need_blit:
-            self.blit()
-            self.need_blit = False
+        if self.invalidated_rects:
+            for r in self.invalidated_rects:
+                self.comp_surf.blit(self.surf, r)
+            for layer in self.layers:
+                for child in layer:
+                    child.redraw_if_needed()
+                    for rect in self.invalidated_rects:
+                        if rect.colliderect(child.rect):
+                            r = rect.move((-child.rect.left, -child.rect.top))
+                            child.blit_on_parent(r.clip(child.rect))
+            for r in self.invalidated_rects:
+                self.blit_on_parent(r)
+            self.invalidated_rects = []
             return True
         return False
 
-    def handle_event(self, ev):
-        handled = False
-        for child in self.children:
-            handled = child.handle_event(ev) or handled
-        return handled
-
-    def invalidate(self):
-        self.need_blit = True
+    def blit_on_parent(self, area=None):
+        # area is a Rect in local space
+        if not area:
+            area = self.surf.get_rect()
         if self.parent:
-            self.parent.invalidate()
+            self.parent.surf.blit(self.surf, area.move(self.rect.topleft), area)
+    #endregion
 
-    def blit(self):
-        if self.parent:
-            self.parent.surf.blit(self.surf, self.rect)
-
+    #region Transformations
     def transform_point(self, point):
         # local space --> screen space
         p = tuple(point[i] * self.rect.size[i] // self.surf.get_rect().size[i] + self.rect[i] for i in range(2))
@@ -113,6 +141,7 @@ class GameObject(object):
         r.topleft = self.inverse_transform_point(r.topleft)
         r.size = tuple(r[i] * self.rect[i] // self.rect[i] for i in range(2))
         return r
+    #endregion
 
     def touching_mouse(self):
         return self.transform_rect(self.surf.get_rect()).collidepoint(pygame.mouse.get_pos())
@@ -121,8 +150,11 @@ class Display(GameObject):
     def __init__(self, *args, **kwargs):
         super().__init__(pygame.display.set_mode(*args, **kwargs))
 
-    def blit(self):
-        pygame.display.flip()
+    def blit_on_parent(self, area=None):
+        if area:
+            pygame.display.update(area)
+        else:
+            pygame.display.flip()
 
 class Text(GameObject):
     def __init__(self, *args, **kwargs):
@@ -145,9 +177,9 @@ class TextButton(GameObject):
         self.idle_surf = text_engine.render_text(font, text, fg=fg, bg=bg, *args, **kwargs)
         self.hover_surf = text_engine.render_text(font, text, fg=fg_hover, bg=bg_hover, *args, **kwargs)
         self.click_surf = text_engine.render_text(font, text, fg=fg_click, bg=bg_click, *args, **kwargs)
-        super().__init__(self.idle_surf)
         self.click_handler = click_handler
         self.pressed = False
+        super().__init__(self.idle_surf)
 
     def update(self):
         super().update()
@@ -173,3 +205,27 @@ class TextButton(GameObject):
                     self.pressed = False
                     return True
         return handled
+
+class Rotatable(GameObject):
+    def __init__(self, image_name_pattern, angle_increment=15):
+        self.angle_increment = angle_increment
+        self.surfs = [pygame.image.load(image_name_pattern.format(i)) for i in range(0, 90, angle_increment)]
+        self.rot = 0
+        super().__init__(self.surfs[0])
+
+    def set_rotation(self, angle):
+        self.rot = angle
+        self.invalidate()
+
+    def get_rotation(self):
+        return self.rot
+
+    def rotate(self, angle):
+        self.rot = (self.rot + angle) % 360
+        self.invalidate()
+
+    def invalidate(self):
+        a = round(self.rot / self.angle_increment) * self.angle_increment % 90
+        b = round((self.rot - a) / 90) * 90
+        self.surf = pygame.transform.rotate(self.surfs[round(a // 15)], b)
+        super().invalidate()
--- main.py
+++ main.py
@@ -16,7 +16,9 @@ pygame.init()
 size = width, height = 1600, 900
 # size = width, height = 1920, 1080
 # flags = pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.NOFRAME
-flags = pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.NOFRAME
+# flags = pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.NOFRAME
+flags = pygame.FULLSCREEN | pygame.HWSURFACE | pygame.NOFRAME
+flags = pygame.HWSURFACE | pygame.NOFRAME
 speed = [2, 2]
 black = 0, 0, 0
 
@@ -39,6 +41,13 @@ button1 = game_objects.TextButton('big_button', 'Button!', lambda: print('pushy 
 button1.move_center_to((disp.rect[2] // 2, disp.rect[3] // 2))
 disp.add_child(button1, layers.UI_FG)
 
+rock = game_objects.Rotatable('img/handle/handle-red-{}.png')
+rock.move_center_to((disp.rect[2] // 5, disp.rect[3] // 2))
+# rock.surf = pygame.transform.smoothscale(rock.surf, tuple(rock.rect.size[i] * 2 for i in range(2)))
+# rock.surf = pygame.transform.rotate(rock.surf, 90)
+disp.add_child(rock, layers.SPRITE_0)
+
+
 # Main loop
 while True:
 
@@ -48,12 +57,15 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN and event.key in (pygame.K_DELETE, pygame.K_BACKSPACE, pygame.K_ESCAPE):
+            # print(clock.get_fps())
             sys.exit()
 
     # Update objects
+    rock.rotate(1)
     disp.pre_update()
     disp.update()
     disp.post_update()
+    # background.invalidate()
 
     # Render screen
     disp.redraw_if_needed()
